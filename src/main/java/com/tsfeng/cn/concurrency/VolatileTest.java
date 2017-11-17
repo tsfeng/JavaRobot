f0f1466dedae8e8dbf261b3b779bf91c616fc28f
tsfeng@126.com <tsfeng@126.com>
2017-11-17 18:20:23 +0800

    volatile

---
+++ src/main/java/com/tsfeng/cn/concurrency/VolatileTest.java
@@ -0,0 +1,9 @@
+package com.tsfeng.cn.concurrency;
+
+/**
+ * @author tsfeng
+ * @version 创建时间 2017/11/17 14:40
+ */
+public class VolatileTest {
+
+}
--- src/main/java/com/tsfeng/cn/design/singleton/dbcheck/Singleton.java
+++ src/main/java/com/tsfeng/cn/design/singleton/dbcheck/Singleton.java
@@ -38,12 +38,27 @@ package com.tsfeng.cn.design.singleton.dbcheck;
  * 所以线程二会直接返回 instance，然后使用，然后顺理成章地报错。
  * 我们只需要将 instance 变量声明成 volatile 就可以了。
  *
+ * 有些人认为使用 volatile 的原因是可见性，也就是可以保证线程在本地不会存有 instance 的副本，每次都是去主内存中读取。
+ * 但其实是不对的。使用 volatile 的主要原因是其另一个特性：禁止指令重排序优化。
+ * 也就是说，在 volatile 变量的赋值操作后面会有一个内存屏障（生成的汇编代码上），读操作不会被重排序到内存屏障之前。
+ * 比如上面的例子，取操作必须在执行完 1-2-3 之后或者 1-3-2 之后，不存在执行到 1-3 然后取到值的情况。
+ * 从「先行发生原则」的角度理解的话，就是对于一个 volatile 变量的写操作都先行发生于后面对这个变量的读操作（这里的“后面”是时间上的先后顺序）。
+ *
+ * 但是特别注意在 Java 5 以前的版本使用了 volatile 的双检锁还是有问题的。
+ * 其原因是 Java 5 以前的 JMM （Java 内存模型）是存在缺陷的，即时将变量声明成 volatile 也不能完全避免重排序，
+ * 主要是 volatile 变量前后的代码仍然存在重排序问题。这个 volatile 屏蔽重排序的问题在 Java 5 中才得以修复，
+ * 所以在这之后才可以放心使用 volatile。
+ *
  */
 public class Singleton {
 
 	private volatile static Singleton instance = null;
 
-	private Singleton(){};
+	private Singleton(){
+		if (instance != null) {
+			throw new IllegalStateException("Already initialized.");
+		}
+	};
 
 
 	public static Singleton getInstance(){
@@ -58,5 +73,24 @@ public class Singleton {
 			}
 		}
 		return instance;
+	}
+
+    /**
+     * local variable increases performance by 25 percent
+     */
+	public static Singleton getInstance2(){
+		Singleton result = instance;
+		//先检查实例是否存在，如果不存在才进入下面的同步块
+		if(result == null){
+			//同步块，线程安全的创建实例
+			synchronized (Singleton.class) {
+				result = instance;
+				//再次检查实例是否存在，如果不存在才真正的创建实例
+				if(result == null){
+					result = instance = new Singleton();
+				}
+			}
+		}
+		return result;
 	}
 }
