2f95c90af8545b6794f7336b661aa4d3965f7477
tsfeng@126.com <tsfeng@126.com>
2017-11-27 18:26:03 +0800

    demo

---
+++ src/main/java/com/tsfeng/cn/core/map/MapDemo.java
@@ -0,0 +1,28 @@
+package com.tsfeng.cn.core.map;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author tsfeng
+ * @version 创建时间 2017/11/27 16:06
+ * Map(interface)
+ * SortedMap(interface)
+ * NavigableMap(interface)
+ * AbstractMap(abstract class)
+ * IdentityHashMap(class)
+ * Hashtable(class)
+ * HashMap(class)
+ * WeakHashMap(class)
+ * EnumMap(class)
+ * TreeMap(class)
+ * Properties(class)
+ * LinkedHashMap(class)
+ */
+public class MapDemo {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+//        ConcurrentHashMap
+    }
+}
